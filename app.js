04640ebcdb295670229f2ac197fff70f84a0c274
Elias Niemi <niemif2@gmail.com>
2020-09-15 11:03:30 +0300

    toiminnalisuus

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 'use strict';
 
+submit.onclick = function() {
+    const username = document.querySelector("#username").value;
+    const password = document.querySelector("#password").value;
+
+    const tiedot = document.querySelector("#tiedot");
+
+    tiedot.innerHTML = `${username} ${password}`;
+    
+    return false;
+}
